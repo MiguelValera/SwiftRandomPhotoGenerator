38a515ef0dafd1213de339b8bf27fecf9b860d81
Miguel Valera <correomiguelvalera@gmail.com>
2022-07-25 00:29:08 +0200

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Miguel Valera Salmerón on 25/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
